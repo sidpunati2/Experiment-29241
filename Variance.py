a069d9869c3350afdc54395e2a8dd6775a3819b3
Nicholas Minor <nrminor@wisc.edu>
2023-10-11 15:35:20 -0500

    adding some good python conventions to script

---
+++ Variance.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Variance.py prints "Hello world!"
+
+Usage:
+    python3 Variance.py
+"""
+
+def main() -> None:
+    """
+        Main prints 'Hello world!'.
+    
+    Args:
+        No arguments
+    
+    Returns:
+        None
+    """
+
+    print("Hello world!")
+
+if __name__ == "__main__":
+    main()
