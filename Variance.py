ae32d752c1545d81bf6364b0a2a1c765f0cea551
sidpunati2 <147658899+sidpunati2@users.noreply.github.com>
2023-11-10 15:37:59 -0600

    Update Variance.py

--- Variance.py
+++ Variance.py
@@ -9,7 +9,8 @@ Usage:
 
 def main() -> None:
     """
-        Main prints 'Hello world!'.
+        Assembles a dataframe highlighting highest locations of mutations
+        and mutation type
     
     Args:
         No arguments
@@ -18,7 +19,68 @@ def main() -> None:
         None
     """
 
-    print("Hello world!")
+    import pandas as pd
+    
+    def parse(s: str, b: bool = True) -> []:
+        hold = []
+        current = []
+        abuild = None
+        nbuild = None
+
+        if s == '()' or s == None:
+            return [[None, None, None, None]]
+        
+        muts = s.strip('()').split(',')
+
+        if b == False:
+            return muts
+
+        for mut in muts:
+            current.append(mut)
+            for v in mut:
+                if v == '_':
+                    current.append(abuild)
+                    abuild = None
+                elif v.isalpha():
+                    if abuild == None:
+                        abuild = v
+                        continue
+                    if nbuild != None:
+                        current.append(abuild)
+                        abuild = v
+                        nbuild = None
+                        continue
+                    abuild = abuild + v
+                elif v.isdigit():
+                    if nbuild == None:
+                        nbuild = v
+                        abuild = abuild + v
+                        continue
+                    nbuild = nbuild + v
+                    abuild = abuild + v
+            current.append(abuild)
+            hold.append(current)
+            current = []
+            abuild = None
+
+        return hold
+        
+    original = pd.read_csv('africa_high_distance_candidates.tsv', delimiter = '\t')
+    dic = {}
+
+    mutations = original[['AA Substitutions']]
+    mutations['mutations'] = mutations['AA Substitutions'].apply(parse)
+
+    for i in range(len(mutations['mutations'])):
+        for mut in mutations['mutations'][i]:
+                print(mut)
+                name, pro, orig, sub = mut
+                if name not in dic.keys():
+                    break
+
+
+
+
 
 if __name__ == "__main__":
     main()
